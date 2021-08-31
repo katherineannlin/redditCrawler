3fd9d5f3c6df740791338becc05bcafc81875ded
Katherine Lin <katherinelin@Katherines-MacBook-Pro.local>
2021-09-01 00:25:56 +0800

    Remove comments.

--- Puppeteer-scraper/index.js
+++ Puppeteer-scraper/index.js
@@ -1,14 +1,11 @@
 import startBrowser from './browser.js';
 import Scraper from './scraper.js';
 
-// const scraperController = require('./pageController');
-
 //Start the browser and create a browser instance
 const browserInstance = await startBrowser();
 const scraperInsance = new Scraper(browserInstance);
 
 // Pass the browser instance to the scraper controller
-// scraperController(browserInstance)
 await scraperInsance.open('http://www.reddit.com/r/titanfolk/');
 const keywords = ['Levi', 'Erwin'];
 const page_limit = 5;
